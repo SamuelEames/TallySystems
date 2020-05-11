aa01c47c5f3daa04bcd94ad1ac71d5b142df629d
Samuel Eames <samueljeames@gmail.com>
2020-05-12 00:24:32 +1000

    Added FastLED library

--- vMixTallyLights.ino
+++ vMixTallyLights.ino
@@ -6,8 +6,25 @@
  * Modified by Arduino LLC (2015)
  */ 
 
+#include "FastLED.h"
 #include "MIDIUSB.h"
 
+// Pixel Setup
+#define NUM_LEDS 12
+#define DATA_PIN 21
+CRGB leds[NUM_LEDS]; // Define the array of leds
+
+#define COL_RED     0xFF0000
+#define COL_ORANGE  0xFF2800
+#define COL_YELLOW  0xFF8F00
+#define COL_GREEN   0x00FF00
+#define COL_BLUE    0x0000FF
+#define COL_PURPLE  0xB600FF
+#define COL_WHITE   0xFFFF7F
+#define COL_BLACK   0x000000
+
+
+// MIDI Setup
 // First parameter is the event type (0x09 = note on, 0x08 = note off).
 // Second parameter is note-on/note-off, combined with the channel.
 // Channel can be anything between 0-15. Typically reported to the user as 1-16.
@@ -15,17 +32,27 @@
 // Fourth parameter is the velocity (64 = normal, 127 = fastest).
 
 void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOn);
+	midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+	MidiUSB.sendMIDI(noteOn);
 }
 
 void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOff);
+	midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+	MidiUSB.sendMIDI(noteOff);
 }
 
 void setup() {
-  Serial.begin(115200);
+	Serial.begin(115200);
+
+
+	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+	FastLED.setMaxPowerInVoltsAndMilliamps(5,120);
+
+	for (int i = 0; i < NUM_LEDS; ++i)
+		leds[i] = COL_WHITE;
+
+	FastLED.show();
+
 }
 
 // First parameter is the event type (0x0B = control change).
@@ -34,23 +61,35 @@ void setup() {
 // Fourth parameter is the control value (0-127).
 
 void controlChange(byte channel, byte control, byte value) {
-  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
-  MidiUSB.sendMIDI(event);
+	midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+	MidiUSB.sendMIDI(event);
 }
 
-void loop() {
-  midiEventPacket_t rx;
-  do {
-    rx = MidiUSB.read();
-    if (rx.header != 0) {
-      Serial.print("Received: ");
-      Serial.print(rx.header, HEX);
-      Serial.print("-");
-      Serial.print(rx.byte1, HEX);
-      Serial.print("-");
-      Serial.print(rx.byte2, HEX);
-      Serial.print("-");
-      Serial.println(rx.byte3, HEX);
-    }
-  } while (rx.header != 0);
+void loop() 
+{
+	midiEventPacket_t rx;
+	do 
+	{
+		rx = MidiUSB.read();
+		if (rx.header != 0) 
+		{
+			Serial.print("Received: ");
+			Serial.print(rx.header, HEX);
+			Serial.print("-");
+			Serial.print(rx.byte1, HEX);
+			Serial.print("-");
+			Serial.print(rx.byte2, HEX);
+			Serial.print("-");
+			Serial.println(rx.byte3, HEX);
+		}
+	} while (rx.header != 0);
+
+	if (rx.byte3 == 0x7F)
+	{
+		leds[0] = COL_RED;
+	}
+	else
+		leds[0] = COL_GREEN;
+
+	FastLED.show();
 }
