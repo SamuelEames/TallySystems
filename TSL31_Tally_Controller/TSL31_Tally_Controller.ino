81da474989c45365522c046b8df151ac6a379c24
Samuel Eames <samueljeames@gmail.com>
2022-02-16 23:25:11 +1000

    Tidied up & fixed bugs

--- TSL31_Tally_Controller/TSL31_Tally_Controller.ino
+++ TSL31_Tally_Controller/TSL31_Tally_Controller.ino
@@ -2,10 +2,16 @@
 
 
 TODO
- * Change NRF24L01 to do broadcast rather than unicast
-    * to improve speed of update
-    * Make infinitely epandable 
- * Change to lower bitrate - to try and improve reliabality
+ * Try to improve speed
+      * Controller seems to be receiving everything, but tally lights aren't getting every update, so lag behind sometimes
+      * Implement brightness
+
+ * Boss level
+    * Implement screen and encoder/button knob for setting paremeters
+          * IP Address
+          * Port Number
+          * Brightness (controller LEDs, Front tally lights, back tally lights)
+          * ISO Bus number to respond to
 
 
 
@@ -23,9 +29,16 @@ RADIO DATA FORMAT
   Switcher Tally Setup - notes just for me because I keep forgetting them
    * 1-tslumd_1.0
    * Multicast IP 224.0.168.168
-   * Port: 8903
-   * FSLBL ON
-   * ShowBusName Off
+
+   * 1SecUpdate   On      -- Updates one tally every (when idle) rather than dump of everything once a minute
+   * ShowUMDIf    On      -- Essential for ISO feature to work
+   * ShowBusName  On      -- Not essential, but handy for debugging
+   * FSLBL        On      -- I have no idea what this means - it shows '(FS)' on inputs 
+   * Center       Off     -- Not sure if this is critical anymore -- centers text, otherwise text is left justified
+   * Transport    UDP     -- Important 
+   * Port         8903    -- Important
+
+
 
 */
 
@@ -33,7 +46,7 @@ RADIO DATA FORMAT
 
 
 // SETUP DEBUG MESSAGES
-#define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
+// #define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
 #ifdef DEBUG
   #define DPRINT(...)   Serial.print(__VA_ARGS__)   //DPRINT is a macro, debug print
   #define DPRINTLN(...) Serial.println(__VA_ARGS__) //DPRINTLN is a macro, debug print with new line
@@ -82,18 +95,11 @@ CRGB leds[NUM_LEDS]; // Define the array of leds
 RF24 radio(RF_CE_PIN, RF_CSN_PIN);
 
 uint8_t RF_address[] = "TALY0";
-// uint8_t addrStartCode[] = "TALY";          // First 4 bytes of node addresses (5th byte on each is node ID - set later)
-// uint8_t nodeAddr[MAX_TALLY_NODES][5];  
-// bool nodePresent[MAX_TALLY_NODES];         // Used to indicate whether a receiving node responded to a message or not
-
 
 #define RF_BUFF_LEN 7                     // Number of bytes to transmit / receive -- Prog RGB, Prev RGB
 // uint8_t radioBuf_RX[RF_BUFF_LEN];
 uint8_t radioBuf_TX[RF_BUFF_LEN];
 bool newRFData = false;                   // True if new data over radio just in
-
-// uint8_t myID = 0;                          // master = 0
-
 
 
 //////////////////// ETHERNET SETUP ////////////////////
@@ -117,7 +123,7 @@ uint16_t tallyState_ALL[4];                   // Holds current state of all tall
 
 
 #define tallyBrightness 7                 // Brightness of tally lights on nodes (range 0-15)
-#define REFRESH_INTERVAL  1000            // (ms) retransimits tally data to nodes when this time elapses from last transmit 
+#define REFRESH_INTERVAL  200            // (ms) retransimits tally data to nodes when this time elapses from last transmit 
                                             // Note: A transmit is executed every time a change in tally data is detected
 uint32_t lastTXTime = 0;                  // Time of last transmit
 
@@ -128,47 +134,26 @@ bool frontTallyON = true;
 
 void setup() 
 {
+  // INITIALISE PIXEL LEDS
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  fill_solid(leds, NUM_LEDS, COL_BLUE); // Clear LEDs
+  FastLED.setBrightness(LED_BRIGHTNESS);
+  FastLED.show();
+
+
   #ifdef DEBUG
     Serial.begin(115200);
     while (!Serial) ; 
   #endif
 
-  
   // INITIALISE RADIO
-
-  // // Generate node addresses
-  // for (uint8_t i = 0; i < MAX_TALLY_NODES; ++i)
-  // {
-  //  for (uint8_t j = 0; j < 4; ++j) // Set first four bytes of address to same code
-  //    nodeAddr[i][j] = addrStartCode[j];
-
-  //  nodeAddr[i][4] = i;         // Unique 5th byte according to node address
-  // }
-
-/*  radio.begin();
-  // radio.setChannel(108);         // Keep out of way of common wifi frequencies = 2.4GHz + 0.108 GHz = 2.508GHz
-  radio.setDataRate(RF24_250KBPS);    // Let's make this quick
-  // radio.enableDynamicPayloads();
-  // radio.stopListening();             // Probably don't need this line, but eh
-  // radio.setAutoAck(0, false); 
-  radio.setRetries(0,5);          // Smallest time between retries (delay, count)
-  radio.openWritingPipe(RF_address);  // Broadcast to ALLL tallies
-  // Note: writing pipe opened when we know who we want to talk to*/
-
   radio.begin();
   radio.setChannel(108);              // Keep out of way of common wifi frequencies = 2.4GHz + 0.108 GHz = 2.508GHz
   radio.setPALevel(RF24_PA_MAX);      // Let's make this powerful... later (RF24_PA_MAX)
   radio.setDataRate( RF24_250KBPS );  // Use low bitrate to be more reliable.. I think??
   radio.setAutoAck(false);            // Don't acknowledge messages
-  // radio.setRetries(3,5); // delay, count
   radio.openWritingPipe(RF_address);  // All messages sent to same address
 
-
-  // INITIALISE PIXEL LEDS
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  fill_solid(leds, NUM_LEDS, COL_BLACK); // Clear LEDs
-  FastLED.setBrightness(LED_BRIGHTNESS);
-  FastLED.show();
 
   for (uint8_t i = 0; i < TALLY_QTY; ++i) // clear tally light array
     tallyState_ALL[i] = INPUTOFF;
@@ -221,15 +206,8 @@ void loop()
   if (millis() < lastTXTime)            // Millis() wrapped around - restart timer
     lastTXTime = millis();
 
-  // #ifdef DEBUG 
-  //  PrintTallyArray();        // Print to serial on receiving serial data
-  // #endif
-
   
   LightLEDs_EXTTally();     // light up local LEDs
-
-
-
 
 }
 
@@ -237,63 +215,11 @@ void loop()
 void TX_Tallies()
 {
   // Broadcasts tally data to tally lights
-  if (radio.write( &tallyState_ALL, RF_BUFF_LEN, true ))
-    ;
-    // DPRINTLN(F("TX Success\n"));
-  else
-    ;
-    // DPRINTLN(F("TX FAIL\n"));
-
+  radio.write( &tallyState_ALL, RF_BUFF_LEN, true );
   lastTXTime = millis();
-
 
   return;
 }
-
-
-
-// void TX_Tallies_V1(uint8_t ID)
-// {
-//  // Transmits tally data to tallies that need it
-
-//  if (ID == 0) // Update ALL tallies
-//  {
-//    DPRINTLN(F("TX TO ALL Tallies\n"));
-//    for (uint8_t i = 1; i < MAX_TALLY_NODES; ++i)
-//    {
-//      // if ((i == 1) || (i == 5) || (i == 6))
-//      //  continue;
-      
-//      radio.stopListening();
-//      radio.openWritingPipe(nodeAddr[i]);
-//      getTXBuf(i);
-
-//      if (radio.write( &radioBuf_TX, RF_BUFF_LEN )) 
-//        nodePresent[i] = true;
-//      else 
-//        nodePresent[i] = false;
-//    }
-
-//    lastTXTime = millis();
-
-//  }
-//  else            // Only update given tally ID
-//  {
-//    DPRINT(F("TX TO TALLY #"));
-//    DPRINTLN(ID, DEC);
-//    radio.stopListening();
-//    radio.openWritingPipe(nodeAddr[ID]);
-//    getTXBuf(ID);
-
-//    if (radio.write( &radioBuf_TX, RF_BUFF_LEN )) 
-//      nodePresent[ID] = true;
-//    else 
-//      nodePresent[ID] = false;
-//  }
-
-
-//  return;
-// }
 
 
 void LightLEDs_EXTTally()
@@ -302,15 +228,20 @@ void LightLEDs_EXTTally()
 
   for (uint8_t i = 1; i < NUM_LEDS; ++i)    // Tally ID 0 is master station -- skip this one
   {
+    leds[i-1] = COL_BLACK;
 
-    if (tallyState_ALL[1] & (1 << i)) // Check program
+    if (tallyState_ALL[1] & (1 << i))         // Check program
       leds[i-1] = COL_RED;
-    else if (tallyState_ALL[2] & (1 << i)) // Check ISO
-      leds[i-1] = COL_YELLOW;
-    else if (tallyState_ALL[0] & (1 << i)) // Check preview
+    else if (tallyState_ALL[0] & (1 << i))    // Check preview
       leds[i-1] = COL_GREEN;
-    else
-      leds[i-1] = COL_BLACK;
+
+    if (tallyState_ALL[2] & (1 << i))         // Check ISO (override preview)
+    {
+      if (tallyState_ALL[1] & (1 << i))       // Check for ISO & Prog state
+        leds[i-1] = COL_ORANGE;
+      else
+        leds[i-1] = COL_YELLOW;               // ISO Only
+    }
   } 
 
   FastLED.show();
@@ -318,41 +249,6 @@ void LightLEDs_EXTTally()
   return;
 }
 
-
-
-
-// void getTXBuf(uint8_t ID)
-// {
-//  // Generates data to transmit to given tally number
-//  /* TALLY BYTE DATA STRUCTURE
-//      Bit 1-2 --> tally state 
-//              0 = PROGRAM
-//              1 = PREVIEW
-//              2 = AUDIOON
-//              4 = INPUTOFF
-//      Bit 3   --> frontTallyON
-//              0 = off
-//              1 = on
-//      Bit 4   --> spare
-//      Bit 5-8 --> Tally Brightness (maybe front tally only & set back locally from light sensor?)
-//  */
-
-//  uint8_t tempVal = 0;
-
-//  // Fix this at some point later.. .but will need to fix light code to match
-//  if (tallyState_ALL[ID] & 0x02)      // Check program
-//    tempVal = 0;
-//  else if (tallyState_ALL[ID] & 0x01) // Check preview
-//    tempVal = 1;
-//  else
-//    tempVal = 3;
-
-//  radioBuf_TX[0] = tempVal;
-//  radioBuf_TX[0] |= frontTallyON << 2;      // flag for whether tally nodes should have front light on or not
-//  radioBuf_TX[0] |= tallyBrightness << 4;     // Brightness of tally lights (range 0-15)
-
-//  return;
-// }
 
 
 #ifdef DEBUG
@@ -396,115 +292,59 @@ void LightLEDs_EXTTally()
     return;
   }
 
-  // void UDP_TallyInfoDump()
-  // {
-  //   // Prints tally info to console
-
-
-  //   uint8_t TallyNum = 0;
-  //   bool TallyPrev = false;
-  //   bool TallyProg = false;
-
-  //   if (packetSize == 18)
-  //   {
-  //     // Get Tally Number
-  //     TallyNum = packetBuffer[0] - 0x80;
-
-  //     Serial.print("Tally Number ");
-  //     Serial.print(TallyNum, DEC);
-
-
-
-  //     // Get Preview State
-  //     TallyPrev = packetBuffer[1] & 0x01;
-
-  //     // Get Program State
-  //     TallyProg = (packetBuffer[1] >> 1) & 0x01;
-
-  //     if (TallyPrev)
-  //       Serial.print(" PREVIEW");
-
-  //     if (TallyProg)
-  //       Serial.print(" PROGRAM");
-
-  //     Serial.println();
-
-  //     // Print name
-  //     Serial.print("DisplayData = ");
-  //     for (uint8_t i = 2; i < 18; ++i)
-  //       Serial.write(packetBuffer[i]);
-
-  //     Serial.println();
-      
-  //   }
-
-
-  //   return;
-  // }
-
 #endif
-
-
 
 
 void unpackTSLTally()
 {
-  uint8_t ISO_InputNum = 0;
   // Gets tally data from UDP packet and writes it into RAW tally array
+  uint8_t BUS_InputNum = 0;                         // Holds input number being sent to PGM I bus
+  uint16_t mask;
 
-  // uint8_t dataTemp = 0;
 
   if (packetSize == 18)                             // Check it's the length we're expecting
   {
     // Get Tally Number
-    TallyNum_lastRX = packetBuffer[0] - 0x80;         // Note starting input count from 0
-    // dataTemp = packetBuffer[1] & 0x03;             // Single out Prev & Program bits
-
+    TallyNum_lastRX = packetBuffer[0] - 0x80;       // Note starting input count from 0
 
     if (TallyNum_lastRX < TALLY_QTY)                // Only record data for tallies in our system
     {
       //prev, prog, iso, brightness
 
-      uint16_t mask = 1 << TallyNum_lastRX;
+      mask = 1 << TallyNum_lastRX;
       
-
-      tallyState_ALL[0] = ((tallyState_ALL[0] & ~mask) | ((packetBuffer[1] & 0x01) << TallyNum_lastRX));
-      tallyState_ALL[1] = ((tallyState_ALL[1] & ~mask) | (((packetBuffer[1] >> 1) & 0x01) << TallyNum_lastRX));
-
-      // tallyState_ALL[TallyNum_lastRX] = packetBuffer[1];
-      Serial.print("Received Tally ");
-      Serial.println(TallyNum_lastRX);
-      Serial.println();
-
+      tallyState_ALL[0] = ((tallyState_ALL[0] & ~mask) | ((packetBuffer[1] & 0x01) << TallyNum_lastRX));          // Preview
+      tallyState_ALL[1] = ((tallyState_ALL[1] & ~mask) | (((packetBuffer[1] >> 1) & 0x01) << TallyNum_lastRX));   // Program
 
       newTallyData = true;  
     }
-    else if (TallyNum_lastRX == 0x77) // PGM ISO Bus
+    else if (TallyNum_lastRX == 0x77 /*|| TallyNum_lastRX == 0x19*/)  // PGM ISO Bus () OR 'ME1 BKGD' (25)
     {
       // Get input number that is going to that bus
-      ISO_InputNum = (packetBuffer[2]-0x30) * 100 + (packetBuffer[3]-0x30) * 10 + (packetBuffer[4]-0x30);
+      BUS_InputNum = (packetBuffer[2]-0x30) * 100 + (packetBuffer[3]-0x30) * 10 + (packetBuffer[4]-0x30);
 
-      Serial.print("Input Number is ");
-      Serial.print(packetBuffer[2]);
-      Serial.print("\t");
-      Serial.print(packetBuffer[3]);
-      Serial.print("\t");
-      Serial.println(packetBuffer[4]);
-      Serial.print("ISONUM = ");
-      Serial.println(ISO_InputNum);
-      // Serial.print("\t");
-      // Serial.print(packetBuffer[4]);
-      // Serial.print("\t");
+      DPRINT("PGM I = ");
+      DPRINTLN(BUS_InputNum);
 
-      tallyState_ALL[2] = 1 << ISO_InputNum;
-      newTallyData = true;
+      if (BUS_InputNum < TALLY_QTY)
+      {
+        if (TallyNum_lastRX == 0x77) // ISO
+          tallyState_ALL[2] = 1 << BUS_InputNum;
+        // else // Program
+        // {
+        //   tallyState_ALL[1] = 1 << BUS_InputNum;
+        // }
+        newTallyData = true;
+      }
     }
-  }
+    // else if (TallyNum_lastRX == 0x19)               // ME 1 BKGD
+    // {
+    //   // Get input number that is going to that bus
+    //   BUS_InputNum = (packetBuffer[2]-0x30) * 100 + (packetBuffer[3]-0x30) * 10 + (packetBuffer[4]-0x30);
 
-  Serial.println("tallyState_ALL");
-  for (uint8_t i = 0; i < 4; ++i)
-  {
-    Serial.println(tallyState_ALL[i], BIN);
+    //   DPRINT("PGM I = ");
+    //   DPRINTLN(BUS_InputNum);
+    // }
   }
 
   return;
